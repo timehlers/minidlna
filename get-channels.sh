4f442b76a4271909a19138901ba6bed1e25c15dc
Tim Ehlers <osmocom@ehlers.info>
2015-04-17 22:35:11 +0200

    added option IP for get-channels.sh

--- get-channels.sh
+++ get-channels.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-#channels=`wget "http://192.168.1.110:8080/api/channel/grid?start=0&limit=9999" -O - 2>/dev/null | sed "s/{/\n/g" | sed "s/}/\n/g" | grep uuid | cut -f 4,10 -d '"' | sed 's/"/ /g'`
-channels=`wget "http://192.168.1.110:8080/playlist/channels" -O - 2>/dev/null | tr -d "\n" | sed "s%http://192.168.1.110:8080/stream/channelid/%;%g" | sed "s/#EXTINF:-1,/\n/g"`
+if [ "$1" = "" ]; then
+ echo "Usage $0 IP"
+ exit 1
+fi
+
+channels=`wget "http://${1}:8080/playlist/channels" -O - 2>/dev/null | tr -d "\n" | sed "s%http://192.168.1.110:8080/stream/channelid/%;%g" | sed "s/#EXTINF:-1,/\n/g"`
 
 if [ "$channels" != "" ]; then
 
